0c2f70b271e17e3871cf799d2bf2e55fe090edd2
dungh <dungh@lab2-23.eng.utah.edu>
2016-10-06 19:49:29 -0600

    initial

--- stopwatch.h
+++ stopwatch.h
@@ -2,6 +2,9 @@
 #define STOPWATCH_H
 
 #include <QMainWindow>
+#include<QTimer>
+#include<QObject>
+
 
 namespace Ui {
 class StopWatch;
@@ -13,7 +16,23 @@ class StopWatch : public QMainWindow
 
 public:
     explicit StopWatch(QWidget *parent = 0);
+    int QHundred = 0;
+    int millisecond = 0;
+    int second = 0;
+    int minute = 0;
+    int startClick = 0;
+
+    QTimer myQTimer;
+
+
     ~StopWatch();
+
+private slots:
+    void start_button_click1st();
+
+    void on_startButton_clicked();
+
+    void on_resetButton_clicked();
 
 private:
     Ui::StopWatch *ui;
